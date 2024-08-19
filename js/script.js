0978e2b74e5c788092bb7b66956874e710741237
Suleidis Leyva <suleidisdanai@gmail.com>
2024-08-19 16:06:05 -0300

    resolucion ejercicio para que muestre nombre y apellido los estudiantes al cargar la pagina

--- js/script.js
+++ js/script.js
@@ -19,3 +19,16 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL)
+  .then(response => {
+    if (!response.ok) {
+    throw new Error(`Error al obtener los datos`)
+    } else {
+      return response.json()
+  }
+  })
+  .then(function (data) { showData(data.students) })
+  .catch(function (error) {
+    console.log('Hubo un problema con la operacion fetch: ', error)
+    container.innerHTML = '<p>Error al cargar los datos</p>'
+  })
